e8ac4f6921ec7e0e502db86f714c39829344b15b
Bob Budowniczy <kkrauza@o2.pl>
2021-01-04 22:41:22 +0100

    Create fiboplot.py

---
+++ fiboplot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+tab = []
+tab.append(0)
+tab.append(1)
+tab.append(1)
+licz=[]
+licz.append(0)
+licz.append(1)
+licz.append(2)
+
+
+def fibo(n):
+
+    for i in range(3,n):
+        tab.append(tab[i-1]+tab[i-2])
+        licz.append(i)
+    print(tab)
+    print(licz)
+    fig, ax = plt.subplots()
+    ax.plot(licz, tab,'.')
+
+    ax.set(xlabel='Kolejny wyraz', ylabel='Wartość',
+           title='Wykres ciagu Fibo')
+    ax.grid()
+
+    fig.savefig("test.png")
+    plt.show()
+
+
+fibo(int(input()))
