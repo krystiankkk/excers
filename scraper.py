54c678be5b2b83329b28a91beaef79ead2d8c738
krystiankkk <68556053+krystiankkk@users.noreply.github.com>
2021-01-06 00:57:40 +0100

    Delete scraper.py

--- scraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
-print(results.prettify())
