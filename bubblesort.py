c6a9e11b5148d33f95fc62a7c6efe84015df849b
Bob Budowniczy <kkrauza@o2.pl>
2021-01-05 18:14:53 +0100

    bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,6 @@
+def bubble(list):
+    for i in range(len(list)):
+        for j in range(len(list)-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    print(list)
